95c0139b098f6e2e8f7e5acf0dd713039de60816
PradeepGorkhali <55340536+PradeepGorkhali@users.noreply.github.com>
2023-01-09 14:08:18 +0545

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -33,7 +33,7 @@ function index() {
             <span className="contact_advertisement_wrapper">
               <GoLocation />
             </span>
-            <span>Labim , Lalitpur</span>
+            <span>Labim Marga , Lalitpur</span>
           </a>
         </div>
         <div className="advertisement_second_wrapper_content">
